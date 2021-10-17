bb71d7b5b91d6bf0702f48eaf5a55e01c422b1c1
FrancoCarrion <franco.carrion99@gmail.com>
2021-10-17 11:56:09 -0300

    Dominio
    Commit Con el dominio del programa

---
+++ Dominio/Carga.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrasnporteDeCargas.Dominio
+{
+    public class Carga
+    {
+        public double Peso { get; set; }
+        public string Tipo { get; set; }
+
+        public Carga()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Tipo.ToString() + "-" + Peso;
+        }
+
+
+
+
+
+    }
+}
